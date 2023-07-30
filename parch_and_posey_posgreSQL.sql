a819a375d46e66b3c9e5cd9bf0e10ab108942562
Mosiur Rahaman <masiurreheman01@gmail.com>
2023-07-30 02:34:44 +0200

    With this set of excercise i learned about how ERD model can be very helpful

--- parch_and_posey_posgreSQL.sql
+++ parch_and_posey_posgreSQL.sql
@@ -213,3 +213,36 @@ WHERE accounts.name = 'Walmart'
 
 --Provide a table that provides the region for each sales_rep along with their associated accounts. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
 
+-- Here i use the alias as follows without the AS keywords.
+	 r for region,
+	 s for sales_reps,
+	 a for accounts
+
+SELECT 
+	r.name AS region_name,
+	s.name AS sales_rep_name,
+	a.name AS accounts_name
+FROM sales_reps s
+JOIN region r
+	ON s.region_id = r.id
+JOIN accounts a
+	ON s.id = a.sales_rep_id
+ORDER BY a.name
+
+
+--Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. Your final table should have 3 columns: region name, account name, and unit price. A few accounts have 0 for total, so I divided by (total + 0.01) to assure not dividing by zero.
+
+--In this example i learned about how ERD model is so helpful for to conncet and find the data we need. With the help of ERD model we can easily see the connecting between the data tables and their FK and PK.
+
+SELECT 
+	region.name AS region_name,
+	accounts.name AS account_name,
+	orders.total_amt_usd/(orders.total + 0.01) AS unit_price
+	
+FROM region
+JOIN sales_reps
+	ON region.id = sales_reps.region_id
+JOIN accounts
+	ON sales_reps.id = accounts.sales_rep_id
+JOIN orders
+	ON accounts.id = orders.account_id

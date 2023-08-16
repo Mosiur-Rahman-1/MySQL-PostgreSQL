e32a2c5670e2fe0b7f9462da40d3875f2cebce08
Mosiur Rhaman <masiurreheman01@gmail.com>
2023-08-16 04:46:46 +0200

    Starting to doing complex query with GROUP BY, JOINS and AGGREGATIONS

--- parch_and_posey_posgreSQL.sql
+++ parch_and_posey_posgreSQL.sql
@@ -4,7 +4,10 @@ FROM web_events
 LIMIT 15
 
 
--- ORDER BY
+-- ### ORDER BY
+
+
+
 
 SELECT channel, account_id, occurred_at
 FROM web_events
@@ -178,7 +181,9 @@ WHERE (name LIKE 'C%' OR name LIKE 'W%')
 
 
 
--- SQL Joins
+-- ##### SQL Joins
+
+
 
 -- Try pulling all the data from the accounts table, and all the data from the orders table.
 SELECT *
@@ -251,7 +256,9 @@ JOIN orders
 
 
 
---JOINs and Filtering
+-- #### JOINs and Filtering
+
+
 
 
 
@@ -379,7 +386,10 @@ ORDER BY orders.occurred_at
 
 
 
--- SQL Aggregations
+-- ##### SQL Aggregations
+
+
+
 
 -- Find the total amount of poster_qty paper ordered in the orders table.
 SELECT SUM(poster_qty)
@@ -403,3 +413,68 @@ FROM orders;
 
 SELECT SUM(standard_amt_usd)/SUM(standard_qty) AS standard_price_per_unit
 FROM orders;
+
+-- When was the earliest order ever placed? You only need to return the date.
+
+SELECT MIN(occurred_at) AS earliest_date_order_place
+FROM orders
+
+-- Try performing the same query as in question 1 without using an aggregation function.
+SELECT occurred_at
+FROM orders
+ORDER BY occurred_at
+LIMIT 1
+
+-- When did the most recent (latest) web_event occur?
+SELECT MAX(occurred_at) AS latest_event_occurred_at
+FROM web_events
+
+-- Try to perform the result of the previous query without using an aggregation function.
+
+SELECT occurred_at AS latest_event_occurred_at
+FROM web_events
+ORDER BY occurred_at DESC
+LIMIT 1
+
+-- Find the mean (AVERAGE) amount spent per order on each paper type, as well as the mean amount of each paper type purchased per order. Your final answer should have 6 values - one for each paper type for the average number of sales, as well as the average amount.
+SELECT 
+	AVG(standard_qty) AS mean_standard_qty,
+	AVG(gloss_qty) AS mean_gloss_qty,
+	AVG(poster_qty) AS mean_poster_qty,
+	AVG(standard_amt_usd) AS mean_standard_amt_usd,
+	AVG(gloss_amt_usd) AS mean_gloss_amt_usd,
+	AVG(poster_amt_usd) AS mean_poster_amt_usd
+FROM orders
+
+
+-- Via the video, you might be interested in how to calculate the MEDIAN. Though this is more advanced than what we have covered so far try finding - what is the MEDIAN total_usd spent on all orders?
+
+
+
+
+
+
+-- #### Questions: GROUP BY
+
+-- Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.
+
+SELECT 
+	name AS account_name,
+	occurred_at AS date
+FROM accounts
+JOIN orders
+	ON accounts.id = orders.account_id
+ORDER BY orders.occurred_at
+LIMIT 1
+
+-- Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name.
+
+-- Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name.
+
+-- Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used.
+
+-- Who was the primary contact associated with the earliest web_event?
+
+-- What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.
+
+-- Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps.

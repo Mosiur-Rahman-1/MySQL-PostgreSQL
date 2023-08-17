4d1e2c980fff861165d040c42f1fec80ef2db944
Mosiur Rhaman <masiurreheman01@gmail.com>
2023-08-17 06:40:41 +0200

    All excercise done with GROUP BY caluse

--- parch_and_posey_posgreSQL.sql
+++ parch_and_posey_posgreSQL.sql
@@ -469,12 +469,67 @@ LIMIT 1
 
 -- Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name.
 
+SELECT
+	accounts.name,
+	SUM(orders.total_amt_usd) AS total_sales_amt_usd
+	
+FROM orders
+JOIN accounts
+	ON orders.account_id = accounts.id
+GROUP BY accounts.name
+ORDER BY total_sales_amt_usd DESC
+
 -- Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name.
+
+SELECT 
+	occurred_at AS date,
+	channel,
+	name
+FROM web_events
+JOIN accounts
+	ON web_events.account_id = accounts.id
+ORDER BY occurred_at DESC
+LIMIT 1
 
 -- Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used.
 
+SELECT 
+	channel,
+	COUNT(channel) AS number_of_time_used
+FROM web_events
+GROUP BY channel
+
 -- Who was the primary contact associated with the earliest web_event?
+
+SELECT 
+	primary_poc,
+	occurred_at
+FROM web_events
+JOIN accounts
+	ON web_events.account_id = accounts.id
+ORDER BY occurred_at
+LIMIT 1
 
 -- What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.
 
+SELECT 
+	name,
+	MIN(total_amt_usd) AS total_usd
+FROM orders
+JOIN accounts
+	ON orders.account_id = accounts.id
+GROUP BY name
+ORDER BY total_usd
+
 -- Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps.
+
+SELECT 
+	region.name AS region_name,
+	COUNT(sales_reps.id) AS total_sales_reps
+FROM accounts
+JOIN sales_reps
+	ON accounts.sales_rep_id = sales_reps.id
+JOIN region
+	ON sales_reps.region_id = region.id
+GROUP BY region.name
+ORDER BY total_sales_reps
